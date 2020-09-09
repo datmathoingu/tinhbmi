d2bccdbe41b631894ab416611f9b01aa88b9b453
Thanh Dat <70925557+datmathoingu@users.noreply.github.com>
2020-09-09 16:00:03 +0700

    Update chuyển qua dạng phương thức

--- Program.cs
+++ Program.cs
@@ -1,34 +1,37 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace tinhbmi
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.OutputEncoding = Encoding.UTF8;
-			double height;
-			double weight;
-			double bmi;
-			//Chiều cao: cm
-			//Cân nặng: kg
-			Console.WriteLine("Nhập chiều cao : ");
-			height = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Nhập cân nặng : ");
-			weight = Convert.ToDouble(Console.ReadLine());
-			height = height / 100;
-			bmi = weight / (height * height);
-			Console.WriteLine("BMI của bạn là:{0}", Math.Round(bmi, 4));
-			if (bmi < 18.5)
-				Console.WriteLine("Underweight.");
-			else if (bmi >= 18.5 && bmi < 25)
-				Console.WriteLine("Normal");
-			else if (bmi >= 25 && bmi < 30)
-				Console.WriteLine("Overweight");
-			else if (bmi >= 30)
-				Console.WriteLine("Obese");
-
-		}
-	}
+    class Program
+    {
+        double Tinh(double a, double b)
+        {
+            a = a / 100;
+            double result = b / (a * a);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double height, weight, bmi;
+            //Chiều cao: cm
+            //Cân nặng: kg
+            Console.WriteLine("Nhập chiều cao : ");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhập cân nặng : ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Program n = new Program();
+            bmi = n.Tinh(height, weight);
+            Console.WriteLine("BMI của bạn là: {0}", Math.Round(bmi, 4));
+            if (bmi < 18.5)
+                Console.WriteLine("Bạn xếp vào loại: Underweight.");
+            else if (bmi >= 18.5 && bmi < 25)
+                Console.WriteLine("Bạn xếp vào loại: Normal");
+            else if (bmi >= 25 && bmi < 30)
+                Console.WriteLine("Bạn xếp vào loại: Overweight");
+            else if (bmi >= 30)
+                Console.WriteLine("Bạn xếp vào loại: Obese");
+        }
+    }
 }
